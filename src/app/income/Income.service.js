16e3ea0cf6ef14c79fa80132dfcb6a3bbbbc1fa5
XavierIV <mohd.zafranudin@gmail.com>
2022-01-01 13:52:04 +0800

    Feat: Generic income service

---
+++ src/app/income/Income.service.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const { REACT_APP_API_ENDPOINT: API_ENDPOINT } = process.env;
+
+const URL = `${API_ENDPOINT}/v1/income`;
+
+const fetchAll = function ({ params } = { params: null }) {
+  return axios.get(URL, { params });
+};
+
+const create = function (payload) {
+  return axios.post(URL, payload);
+};
+
+const fetchOne = function (id) {
+  return axios.get(`${URL}/${id}`);
+};
+
+const update = function (id, payload) {
+  return axios.put(`${URL}/${id}`, payload);
+};
+
+const destroy = function (id) {
+  return axios.delete(`${URL}/${id}`);
+};
+
+export default {
+  fetchAll,
+  fetchOne,
+  create,
+  update,
+  destroy,
+};
